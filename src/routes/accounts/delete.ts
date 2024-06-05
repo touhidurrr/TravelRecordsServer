16ab6c436562eea836e9be947aa1c8465a0e08e5
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2024-06-06 04:54:35 +0600

    fix deleteAccount

--- src/routes/accounts/delete.ts
+++ src/routes/accounts/delete.ts
@@ -5,6 +5,9 @@ export const deleteAccount = (app: Elysia) =>
   app.delete(
     "/:accountId",
     async ({ params: { accountId } }) => {
+      await prisma.transaction.deleteMany({
+        where: { accountId },
+      });
       await prisma.account.delete({
         where: { id: accountId },
       });
