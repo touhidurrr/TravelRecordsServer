3089af039ce28ae5e8d291a984abd21da1a0c4f4
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2024-06-06 11:44:36 +0600

    partner

---
+++ src/routes/records/addPartner.ts
@@ -0,0 +1,31 @@
+import { type Elysia, t } from "elysia";
+import { prisma } from "../../services/prisma";
+
+export const addPartner = (app: Elysia) =>
+  app.post(
+    "/:recordId/partners",
+    async ({ error, params: { recordId }, body: { email } }) => {
+      const user = await prisma.user.findFirst({
+        where: { email },
+      });
+
+      if (!user) return error(400, "User not found");
+
+      await prisma.usersOnTravelRecord.create({
+        data: {
+          userId: user.id,
+          travelRecordId: recordId,
+        },
+      });
+
+      return user;
+    },
+    {
+      params: t.Object({
+        recordId: t.Numeric(),
+      }),
+      body: t.Object({
+        email: t.String({ format: "email" }),
+      }),
+    }
+  );
--- src/routes/records/index.ts
+++ src/routes/records/index.ts
@@ -2,8 +2,10 @@ import { Elysia } from "elysia";
 import { getTravelRecords } from "./get";
 import { postTravelRecord } from "./post";
 import { getPartners } from "./getPartners";
+import { addPartner } from "./addPartner";
 
 export const records = new Elysia({ prefix: "/records" })
   .use(getTravelRecords)
   .use(postTravelRecord)
-  .use(getPartners);
+  .use(getPartners)
+  .use(addPartner);
